20a8dbb6e1319a6ec97b0b32621a4032d66c1270
Dustin Rowland <lee.rowland@gmail.com>
2022-03-01 15:52:48 -0600

    added super/subnet prefix change

--- app.py
+++ app.py
@@ -7,14 +7,11 @@ main.py.
 This file starts the flask application.
 """
 
+import json
 import ipaddress
-import re
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
-def calcSubnet(cidr):
-    pass
 
 
 @app.route("/")
@@ -26,23 +23,65 @@ def index():
 @app.route("/ipcalc", methods=["GET", "POST"])
 def ipcalc():
     """Calculate the submitted ip address information."""
-    if request.method == "POST":
+    if (request.method == "POST") and (request.form["newmask"] == ""):
         ipaddr = request.form["ipaddr"]
         netmask = request.form["netmask"]
         subnet = ipaddress.IPv4Network(f"{ipaddr}/{netmask}", strict=False)
+        subnetinfo = ('{'
+                      f'"address": "{ipaddr}",'
+                      f'"netmask": "{netmask}",'
+                      f'"wildcard": "{subnet.hostmask}",'
+                      f'"network": "{subnet.with_prefixlen}",'
+                      f'"broadcast": "{subnet.broadcast_address}",'
+                      f'"hostmin": "{list(subnet.hosts())[0]}",'
+                      f'"hostmax": "{list(subnet.hosts())[-1]}",'
+                      f'"hostnet": "{len(list(subnet.hosts()))}"'
+                      '}')
+        return render_template("ipcalc.html", subnet=json.loads(subnetinfo))
 
-        return render_template("ipcalc.html",
-                               ipaddr=ipaddr,
-                               netmask=netmask,
-                               wildcard=subnet.hostmask,
-                               network=subnet,
-                               bcast=subnet.broadcast_address,
-                               hostmin=list(subnet.hosts())[0],
-                               hostmax=list(subnet.hosts())[-1],
-                               hostnet=subnet.num_addresses)
+    elif (request.method == "POST") and (request.form != ""):
+        ipaddr = request.form["ipaddr"]
+        netmask = request.form["netmask"]
+        subnet = ipaddress.IPv4Network(f"{ipaddr}/{netmask}", strict=False)
+        subnetinfo = ('{'
+                      f'"address": "{ipaddr}",'
+                      f'"netmask": "{netmask}",'
+                      f'"wildcard": "{subnet.hostmask}",'
+                      f'"network": "{subnet.with_prefixlen}",'
+                      f'"broadcast": "{subnet.broadcast_address}",'
+                      f'"hostmin": "{list(subnet.hosts())[0]}",'
+                      f'"hostmax": "{list(subnet.hosts())[-1]}",'
+                      f'"hostnet": "{len(list(subnet.hosts()))}"'
+                      '}')
+
+        newmask = request.form['newmask']
+        newmasks = []
+        if int(newmask) > int(netmask):  # Subnet
+            for newnet in subnet.subnets(new_prefix=int(newmask)):
+                newmasks.append({
+                                "network": f"{newnet.with_prefixlen}",
+                                "broadcast": f"{newnet.broadcast_address}",
+                                "hostmin": f"{list(newnet.hosts())[0]}",
+                                "hostmax": f"{list(newnet.hosts())[-1]}",
+                                "hostnet": f"{len(list(newnet.hosts()))}"
+                                })
+        else:  # Supernet
+            newnet = subnet.supernet(new_prefix=int(newmask))
+            newmasks.append({
+                            "network": f"{newnet.with_prefixlen}",
+                            "broadcast": f"{newnet.broadcast_address}",
+                            "hostmin": f"{list(newnet.hosts())[0]}",
+                            "hostmax": f"{list(newnet.hosts())[-1]}",
+                            "hostnet": f"{len(list(newnet.hosts()))}"
+                            })
+        print(newmasks)
+
+        return render_template("subnets.html", subnet=json.loads(subnetinfo),
+                               newmasks=json.loads(json.dumps(newmasks)))
+
     else:
         return render_template("base.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
